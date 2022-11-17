8f74d8b9ede7e0738b8ea37255a6ba025441e907
synapticonashaikh <ashaikh@synapticon.com>
2022-11-17 14:00:45 +0100

    typedef function pointer

--- co_existance/2test.c
+++ co_existance/2test.c
@@ -43,14 +43,15 @@ void fun(int a)
 void fun1(int a)
 { printf("Value of a is %d\n", a); }
 
+typedef void(*fun_ptr)(int);
 
 void CFunctionPointer(void)
-{
-    
- __attribute__(( fptrgroup("G1") ))void (*fun_ptr)(int) = &fun;
-__attribute__(( fptrgroup("G1") ))void (*fun_ptr1)(int) = &fun1;      
+{    
 
-    (*fun_ptr)(10);
+    __attribute__(( fptrgroup("G1") ))void (*fun_ptr1)(int) = &fun1;  
+    __attribute__(( fptrgroup("G1") ))fun_ptr x;
+    x = fun; 
+    x(10);
     (*fun_ptr1)(20);
 }
 
